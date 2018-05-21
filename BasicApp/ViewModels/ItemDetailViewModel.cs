734485e30cc8580f093cc2f16e0e6f8b3c9cbab1
Nicholas Codalata <nicholascodalata@usicllc.com>
2018-05-21 11:30:22 -0400

    Initial commit

---
+++ BasicApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using BasicApp.Models;
+
+namespace BasicApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Xamarin Forms Test App
+
+After pull Install Node.js 6.3 or later and run the following command.
+--  npm install -g appcenter-cli
